5e10e73a8395f5a8335877700e86ff6a27d35662
bhavya1332 <63660513+bhavya1332@users.noreply.github.com>
2026-04-24 21:37:40 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Middleware
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
